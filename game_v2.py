0755d844c214370ec65dcb184e5daec4a8e27ade
Artem1991 <artem1991nikolaev@yandex.ru>
2023-05-27 16:40:52 +0500

    first commit

---
+++ game_v2.py
@@ -0,0 +1,53 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 1
+    random_predict = 50  # начинаем с середины диапазона
+
+    while number != random_predict:
+        count += 1
+        if number > random_predict:
+            random_predict += (50 // (2 ** (count - 2)))  # смещаем предполагаемое число на половину от диапазона, который соответствует текущему шагу
+        else:
+            random_predict -= (50 // (2 ** (count - 2)))
+    
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
---
+++ skillfactory/bonus1/outliers_lib/example.md
@@ -0,0 +1,38 @@
+# <center> Markdown  
+
+## Markdown
+
+---
+
+### <center> Markdown 
+
+# Markdown23
+
+##### Markdown
+
+* ОВОЩИ
+    * помидоры
+        * короткие
+        * длинные
+            * импорт
+            * отечественные 
+    * огурцы
+     
+# <center>  Котенкин
+<center>  <img src=https://4lapy.ru/resize/1157x660/upload/iblock/04f/04fe59699b8508d498d5110f7ecf263e.jpg width=700 height=300>
+
+***Markdown*** - простой язык разметки.
+
+$$ формула: s = (a+b)/2 - 3^2,$$
+
+где $a=4, b =5$
+
+```python
+
+lst = [10, 34, 21, 21, 3]
+
+summa = sum(lst)
+
+```
+
+> разве ты не помнишь? дни, в которых были мы
